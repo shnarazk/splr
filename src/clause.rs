fadd1d0333e6622424963f9187052f0d075beaed
shujinarazaki <shujinarazaki@protonmail.com>
2018-08-03 17:20:37 +0900

    WIP (src/clause.rs): try to `instance Eq Clause`

--- src/clause.rs
+++ src/clause.rs
@@ -11,16 +11,33 @@ pub enum ClauseKind {
         rank: i32,
         lits: Vec<Lit>,
     },
-    /// Binary clause
-    BiClause(Lit, Lit),
+    // Binary clause
+    // BiClause(Lit, Lit),
 }
+
+impl PartialEq for ClauseKind {
+    fn eq(&self, ref other : &ClauseKind) -> bool {
+        match &self {
+            ClauseKind::NullClause => match other {
+                ClauseKind::NullClause => true,
+                _                      => false,
+            }
+            ClauseKind::Clause { lits: v1, .. } => match &other {
+                ClauseKind::NullClause => false,
+                ClauseKind::Clause { lits: v2, .. } => v1 == v2,
+            }
+        }
+    }
+}
+
+impl Eq for ClauseKind {}
 
 impl fmt::Display for ClauseKind {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match &self {
             ClauseKind::NullClause => write!(f, "Null"),
             ClauseKind::Clause { .. } => write!(f, "a clause"),
-            ClauseKind::BiClause(_, _) => write!(f, "a biclause"),
+//            ClauseKind::BiClause(_, _) => write!(f, "a biclause"),
         }
     }
 }
