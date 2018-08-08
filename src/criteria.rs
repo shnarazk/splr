3a2ab73fde838fb1a6f6d8f7ee6d00b0ed35ee9e
shujinarazaki <shujinarazaki@protonmail.com>
2018-08-08 11:14:03 +0900

    updated restart_condition, #4

--- src/criteria.rs
+++ src/criteria.rs
@@ -4,6 +4,7 @@
 use clause::*;
 use solver::*;
 use std::cmp::max;
+use std::cmp::min;
 use types::*;
 
 const VAR_ACTIVITY_THRESHOLD: f64 = 1e100;
@@ -98,8 +99,8 @@ impl Solver {
         max(1, cnt)
     }
     /// renamed from checkRestartCondition
-    pub fn should_restart(&mut self, lbd: usize, c_l: usize) -> bool {
-        let next = self.next_restart;
+    pub fn should_restart(&mut self, lbd: usize, clv: usize) -> bool {
+        let next: u64 = self.next_restart;
         let count = self.stats[StatIndex::NumOfBackjump as usize] as u64;
         let nas = self.num_assigns() as f64;
         let b_l = self.decision_level() as f64;
@@ -107,22 +108,32 @@ impl Solver {
         let d_s = self.lbd_s.update(lbd as f64);
         let a_f = self.asg_f.update(nas);
         let a_s = self.asg_s.update(nas);
-        self.c_lvl.update(c_l as f64);
+        let c_v = self.c_lvl.update(clv as f64);
         let n_b = self.stats[StatIndex::NumOfBlockRestart as usize];
         let n_f = self.stats[StatIndex::NumOfRestart as usize];
-        let bias = if 5 < n_b && 5 < n_f {
-            0.10 * self.rbias.get()
+        let bias = if min(n_b, n_f) < 4 {
+            1.25
+        } else if n_b <= n_f {
+            1.25 + (n_f as f64 / n_b as f64).powf(2.0)
         } else {
-            0.0
+            1.25 - (n_f as f64 / n_b as f64).powf(2.0)
         };
-        let filled = next <= count;
-        let blocking = (1.25 - bias) * a_s < a_f;
-        let forcing = (1.25 + bias) * d_s < d_f;
-        if !filled {
+        if count < next {
             self.b_lvl.update(b_l);
             false
-        } else {
+        } else if 1.25 * a_s < a_f {
+            self.stats[StatIndex::NumOfBlockRestart as usize] += 1;
+            self.next_restart = count + c_v.powf(2.0) as u64;
+            self.b_lvl.update(b_l);
+            false
+        } else if bias * d_s < d_f {
+            self.stats[StatIndex::NumOfRestart as usize] += 1;
+            self.next_restart = count + (1.5 * c_v) as u64;
             self.b_lvl.update(0.0);
+            true
+        } else {
+            self.next_restart = count + (1.5 * c_v) as u64;
+            self.b_lvl.update(b_l);
             false
         }
     }
