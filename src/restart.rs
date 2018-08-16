3c362cd1dbcce150ba92aa29c6dec1ede5927d43
shujinarazaki <shujinarazaki@protonmail.com>
2018-08-16 21:25:05 +0900

    make some vars holes

--- src/restart.rs
+++ src/restart.rs
@@ -59,7 +59,7 @@ impl Restart for Solver {
     fn should_force_restart(&mut self) -> bool {
         let count = self.stats[Stat::NumOfBackjump as usize] as u64;
         let e_lbd = self.ema_lbd.get();
-        let c_v = self.c_lvl.get();
+        let _c_v = self.c_lvl.get();
         let should_force = 1.0/0.9 < e_lbd;
         if !(count < self.check_restart) && should_force {
             self.next_restart = count + 50;
@@ -77,8 +77,8 @@ impl Restart for Solver {
         let nas = self.num_assigns() as f64;
         let b_l = self.decision_level() as f64;
         let e_asg = self.ema_asg.update(nas);
-        let e_lbd = self.ema_lbd.update(lbd as f64);
-        let c_v = self.c_lvl.update(clv as f64);
+        let _e_lbd = self.ema_lbd.update(lbd as f64);
+        let _c_v = self.c_lvl.update(clv as f64);
         let should_block = 1.1 < e_asg;
         self.b_lvl.update(b_l);
         if !(count < self.check_restart) && should_block {
