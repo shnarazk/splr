07876f7c1d5c3a316d3af7058259b6da88c7e246
shujinarazaki <shujinarazaki@protonmail.com>
2018-08-20 12:43:44 +0900

    add /tests

--- src/clause.rs
+++ src/clause.rs
@@ -26,17 +26,6 @@ pub struct Clause {
     pub learnt: bool,
 }
 
-///```
-///    # use splr::types::*;
-///    let c1 = splr::clause::Clause::new(3, vec![int2lit(1), int2lit(2), int2lit(3)]);
-///    let mut c2 = splr::clause::Clause::new(2, vec![int2lit(-1), int2lit(4)]);
-///    c2.index = 2;
-///    c2.activity = 2.4;
-///    assert_eq!(c1, c1);
-///    assert_eq!(c1 == c1, true);
-///    assert_ne!(c1, c2);
-///    assert_eq!(c2.activity, 2.4);
-///```
 impl PartialEq for Clause {
     fn eq(&self, other: &Clause) -> bool {
         self.index == other.index
---
+++ tests/clause.rs
@@ -0,0 +1,14 @@
+extern crate splr;
+use splr::types::*;
+
+#[test]
+fn clause_equality() -> () { 
+    let c1 = splr::clause::Clause::new(3, vec![int2lit(1), int2lit(2), int2lit(3)]);
+    let mut c2 = splr::clause::Clause::new(2, vec![int2lit(-1), int2lit(4)]);
+    c2.index = 2;
+    c2.activity = 2.4;
+    assert_eq!(c1, c1);
+    assert_eq!(c1 == c1, true);
+    assert_ne!(c1, c2);
+    assert_eq!(c2.activity, 2.4);
+}
