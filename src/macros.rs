d2551225145c0d073fdae5a26cfc97120dd19a85
shujinarazaki <shujinarazaki@protonmail.com>
2018-08-24 00:43:37 +0900

    new file:   src/macros.rs

---
+++ src/macros.rs
@@ -0,0 +1,12 @@
+/// In solver's method, use it like
+/// ```
+/// cref!(self.cv, cid)
+/// ```
+#[macro_export]
+macro_rules! cref {
+    ($cv:expr, $val:expr) => {{
+        match (&mut $cv, &$val) {
+            (v, cid) => &mut v[cid.to_kind()].clauses[cid.to_index()],
+        }
+    }};
+}
