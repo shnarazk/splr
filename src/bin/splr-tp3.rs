512997212d06842e4c00b9f15ac706bc1464fbf0
shujinarazaki <shujinarazaki@protonmail.com>
2018-08-26 09:19:56 +0900

    new file:   src/eliminator.rs

--- src/bin/splr-tp3.rs
+++ src/bin/splr-tp3.rs
@@ -4,7 +4,7 @@ use splr::solver::{Certificate, SatSolver, Solver, Stat};
 use splr::types::EmaKind;
 use std::env;
 
-const VERSION: &str = "Splr-0.0.2, Technical Preview 2";
+const VERSION: &str = "Splr-0.0.3, Technical Preview 3";
 
 fn main() {
     // println!("splr 0.0.1 CARGO_MANIFEST_DIR = {}", env!("CARGO_MANIFEST_DIR"));
---
+++ src/eliminator.rs
@@ -0,0 +1,5 @@
+use clause::Clause;
+use clause::ClauseIdIndexEncoding;
+use clause::ClauseKind;
+use clause_manage::ClauseManagement;
+use solver::{Solver, Stat};
