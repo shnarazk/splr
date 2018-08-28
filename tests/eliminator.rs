b36fba07216cb50c60bb7e2ae6cc56ae0564a147
shujinarazaki <shujinarazaki@protonmail.com>
2018-08-28 16:52:25 +0900

    ditto

--- tests/eliminator.rs
+++ tests/eliminator.rs
@@ -13,23 +13,26 @@ fn check_occurs() {
         num_of_clauses: 10,
         pathname: "".to_string() };
     let mut s = Solver::new(cfg, &cnf);
-    let v = vec![1,2,3];
-    s.add_clause(v);
-    // s.dump("add");
-    let c1 = mkC(1, vec![1,2,3]);
-    let c2 = mkC(2, vec![1,-2,4]);
-    let c3 = mkC(2, vec![1,-2]);
-    let vec = &[&c1, &c2, &c3];
-    for x in vec {
-        for y in vec {
-    println!("{}\tsubsumes\t{}\t=>\t{:?}", x, y, x.subsumes(&y).map(|l| l.int()));
+
+    let c1 = mk_c(1, vec![1,2,3]);
+    let c2 = mk_c(2, vec![1,-2,4]);
+    let c3 = mk_c(2, vec![1,-2]);
+    {
+        let vec = [&c1, &c2, &c3];
+        for x in &vec {
+            for y in &vec {
+                println!("{}\tsubsumes\t{}\t=>\t{:?}", x, y, x.subsumes(&y).map(|l| l.int()));
+            }
         }
     }
+    s.attach_clause(c1);
+    s.attach_clause(c2);
+    s.attach_clause(c3);
+    s.cp.dump("added");
     println!("::done");
 }
 
-
-fn mkC(i: usize, v: Vec<i32>) -> Clause {
+fn mk_c(i: usize, v: Vec<i32>) -> Clause {
     let vec = v.iter().map(|i| int2lit(*i)).collect::<Vec<Lit>>();
     let mut c = Clause::new(ClauseKind::Permanent, false, 0, vec);
     c.index = i;
