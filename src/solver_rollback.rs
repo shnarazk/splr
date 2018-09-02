d79490264db9c82e5e75938dce37bf460252604b
shujinarazaki <shujinarazaki@protonmail.com>
2018-09-02 21:14:47 +0900

    changed K & R

--- src/solver_rollback.rs
+++ src/solver_rollback.rs
@@ -10,9 +10,9 @@ pub trait Restart {
 }
 
 /// for block restart based on average assigments: 1.40
-const R: f64 = 1.15;
+const R: f64 = 1.02;
 /// for force restart based on average LBD of newly generated clauses: 1.15
-const K: f64 = 1.35;
+const K: f64 = 1.28;
 
 impl Restart for Solver {
     /// This function touches:
