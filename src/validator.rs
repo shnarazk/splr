17c136435c80899acb10e43aa32fa49d473a022f
shujinarazaki <shujinarazaki@protonmail.com>
2018-09-03 21:16:53 +0900

    updated validator.rs for dmcr

--- src/validator.rs
+++ src/validator.rs
@@ -8,14 +8,19 @@ impl Solver {
             self.vars[val.abs() as usize].assign = if *val < 0 { LFALSE } else { LTRUE };
         }
     }
-    pub fn validate(&self) -> bool {
+    pub fn validate(&self) -> Option<Vec<i32>> {
         for ck in &KINDS {
             for c in &self.cp[*ck as usize].clauses[1..] {
                 if !self.satisfies(&c) {
-                    return false;
+                    let mut v = Vec::new();
+                    for i in 0..c.len() {
+                        let l = lindex!(c, i);
+                        v.push(l.int());
+                    }
+                    return Some(v);
                 }
             }
         }
-        true
+        None
     }
 }
