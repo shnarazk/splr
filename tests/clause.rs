4af1701ca545b2540650302a668b5909dc50e907
shujinarazaki <shujinarazaki@protonmail.com>
2018-09-04 14:45:48 +0900

    add a test for clause iterator

--- tests/clause.rs
+++ tests/clause.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 extern crate splr;
 use splr::clause::*;
 use splr::types::*;
@@ -22,4 +23,24 @@ fn clause_equality() -> () {
     assert_eq!(c1 == c1, true);
     assert_ne!(c1, c2);
     assert_eq!(c2.activity, 2.4);
+}
+
+#[test]
+fn clause_iterator() -> () {
+    let c1 = splr::clause::Clause::new(
+        ClauseKind::Permanent,
+        false,
+        3,
+        vec![int2lit(1), int2lit(2), int2lit(3)],
+    );
+    let mut total = 0;
+    for i in &c1 {
+        total += i.int();
+    }
+    assert_eq!(total, 6);
+    let mut iter = c1.into_iter();
+    assert_eq!(iter.next(), Some(int2lit(1)));
+    assert_eq!(iter.next(), Some(int2lit(2)));
+    assert_eq!(iter.next(), Some(int2lit(3)));
+    assert_eq!(iter.next(), None);
 }
