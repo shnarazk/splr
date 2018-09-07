d771340e40588fdede1ab675ca60b8c0cef5b769
shujinarazaki <shujinarazaki@protonmail.com>
2018-09-07 21:05:47 +0900

    change parameters

--- src/solver_rollback.rs
+++ src/solver_rollback.rs
@@ -12,9 +12,9 @@ pub trait Restart {
 
 const RESTART_PERIOD: u64 = 60;
 /// for block restart based on average assigments: 1.40
-const R: f64 = 1.05;
+const R: f64 = 1.1;
 /// for force restart based on average LBD of newly generated clauses: 1.15
-const K: f64 = 1.28; // 1.0 / 0.8
+const K: f64 = 1.4; // 1.0 / 0.8
 
 impl Restart for Solver {
     /// This function touches:
