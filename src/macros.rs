94f9bd4fc93909f5e0e516e2f7608dff8e1290ab
shujinarazaki <shujinarazaki@protonmail.com>
2018-09-18 12:52:24 +0900

    changed the definitions of macros

--- src/macros.rs
+++ src/macros.rs
@@ -4,16 +4,16 @@
 /// ```
 #[macro_export]
 macro_rules! iref {
-    ($cv:expr, $val:expr) => {{
-        match (&$cv, &$val) {
+    ($cv: expr, $val: expr) => {{
+        match (&$cv, $val) {
             (v, cid) => &v[cid.to_kind()].clauses[cid.to_index()],
         }
     }};
 }
 
 macro_rules! mref {
-    ($cv:expr, $val:expr) => {{
-        match (&mut $cv, &$val) {
+    ($cv: expr, $val: expr) => {{
+        match (&mut $cv, $val) {
             (v, cid) => &mut v[cid.to_kind()].clauses[cid.to_index()],
         }
     }};
@@ -21,12 +21,23 @@ macro_rules! mref {
 
 macro_rules! lindex {
     ($c:expr, $val:expr) => {{
-        match (&$c, &$val) {
-            (c, i) => match i {
-                0 => c.lit[0],
-                1 => c.lit[1],
-                n => c.lits[n - 2],
-            },
+        match (&$c, $val) {
+            (c, i) => if i < 2 { c.lit[i] } else { c.lits[i - 2] },
         }
     }};
+}
+
+#[allow(unused_macros)]
+/// WARNING: call `set_flag(ClauseFlag::Locked)` by yourself after this function.
+macro_rules! uenqueue {
+    ($vs: expr, $tr: expr, $tl: expr, $lit: expr, $cid: expr) => {{
+        match (&$vs, &mut $tr, &$tl, $lit, $cid) {
+            (vs, tr, tl, lit, cid) => {
+                let mut v = &mut vs[lit.vi()];
+                v.assign = lit.lbool();
+                v.level = tl.len();
+                v.reason = cid;
+                tr.push(lit);
+            }}
+    }}
 }
