e46d32db2f0fb6f1c64cda8b9e076e2d7e74985d
shujinarazaki <shujinarazaki@protonmail.com>
2018-10-15 17:33:31 +0900

    a better filename handling in Profile

--- src/profiler.rs
+++ src/profiler.rs
@@ -1,6 +1,7 @@
 extern crate chrono;
 use self::chrono::*;
 use std::fmt;
+use std::path::Path;
 
 #[derive(Debug)]
 pub struct Profile {
@@ -9,10 +10,10 @@ pub struct Profile {
 }
 
 impl Profile {
-    pub fn new(target: String) -> Profile {
+    pub fn new(fname: String) -> Profile {
         Profile {
             start: Utc::now(),
-            target, 
+            target: Path::new(&fname).file_name().unwrap().to_os_string().into_string().unwrap(),
         }
     }
 }
@@ -21,11 +22,10 @@ impl fmt::Display for Profile {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "{}, {} ({})",
+            "{}| {}, {:?}",
             self.target,
             Utc::now() - self.start,
             self.start,
         )
     }
 }
-
