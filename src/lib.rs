24bf433f96b1a5938dd756e7e8588b080f02b12e
shujinarazaki <shujinarazaki@protonmail.com>
2018-12-11 21:34:35 +0900

    macros in Rust2018

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,38 @@
 //! This is a SAT solver in Rust.
-#[macro_use]
-mod macros;
+
+macro_rules! clause {
+    ($cv: expr, $val: expr) => {{
+        match (&$cv, $val) {
+            (v, cid) => &v[cid.to_kind()].head[cid.to_index()],
+        }
+    }};
+}
+
+#[macro_export]
+macro_rules! clause_mut {
+    ($cv: expr, $val: expr) => {{
+        match (&mut $cv, $val) {
+            (v, cid) => &mut v[cid.to_kind()].head[cid.to_index()],
+        }
+    }};
+}
+
+#[allow(unused_macros)]
+/// WARNING: call `set_flag(ClauseFlag::Locked)` by yourself after this function.
+macro_rules! uenqueue {
+    ($vs: expr, $tr: expr, $tl: expr, $lit: expr, $cid: expr) => {{
+        match (&$vs, &mut $tr, &$tl, $lit, $cid) {
+            (vs, tr, tl, lit, cid) => {
+                let mut v = &mut vs[lit.vi()];
+                v.assign = lit.lbool();
+                v.level = tl.len();
+                v.reason = cid;
+                tr.push(lit);
+            }
+        }
+    }};
+}
+
 // /// Subsumption-based clause/var eliminaiton
 /// Clause
 pub mod clause;
