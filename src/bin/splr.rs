73292cdb49dc8284c9600c77ebea4ebc89f807d6
shujinarazaki <shujinarazaki@protonmail.com>
2018-12-13 16:28:13 +0900

    use structopt in splr

--- src/bin/splr.rs
+++ src/bin/splr.rs
@@ -1,37 +1,43 @@
 // SAT solver for Propositional Logic in Rust
 use splr::solver::{Certificate, SatSolver, Solver};
-use std::env;
 use std::fs::File;
 use std::io::{BufWriter, Write};
-use std::path::Path;
-use std::process::exit;
+use structopt::StructOpt;
 
-const VERSION: &str = "Splr-0.0.11 (Technology Preview 11) by shnarazk@gitlab.com";
+// const VERSION: &str = "Splr-0.0.11 (Technology Preview 11) by shnarazk@gitlab.com";
+
+#[derive(StructOpt)]
+#[structopt(name = "splr", about = "SAT solver for Propositional Logic in Rust, Technology Preview 11")]
+struct CLOpts {
+    #[structopt(long = "forcing-restart", short="K")]
+    k: Option<f64>,
+    #[structopt(long = "blocking-restart", short="R")]
+    r: Option<f64>,
+    #[structopt(long = "tty", short="t")]
+    no_tty: bool,
+    #[structopt(long = "elim", short="e")]
+    no_elim: bool,
+    #[structopt(parse(from_os_str))]
+    cnf: std::path::PathBuf,
+}
 
 fn main() {
-    let mut target: Option<String> = None;
-    let args: Vec<String> = env::args().skip(1).collect();
-    for arg in &args {
-        match arg {
-            _ if arg == "--version" => {
-                println!("{}", VERSION);
-            }
-            _ if (&*arg).starts_with('-') => {
-                if arg == "--help" {
-                    println!("splr [CNF|--version|--help]");
-                    exit(0);
-                }
-                continue;
-            }
-            _ => {
-                target = Some(arg.to_string());
-            }
+    let args = CLOpts::from_args();
+    if args.cnf.exists() {
+        let (mut s, _cnf) = Solver::build(&args.cnf.to_str().unwrap());
+        let result = format!(".ans_{}", args.cnf.file_name().unwrap().to_str().unwrap());
+        if args.no_tty {
+            s.config.use_tty = false;
         }
-    }
-    if let Some(path) = target {
-        let file = Path::new(&path);
-        let (mut s, _cnf) = Solver::build(&path);
-        let result = format!(".ans_{}", file.file_name().unwrap().to_str().unwrap());
+        if args.no_elim {
+            s.eliminator.use_elim = false; 
+        }
+        if let Some(k) = args.k {
+            println!("This version can't handle '-K {:>4.2}'.", k);
+        }
+        if let Some(r) = args.r {
+            println!("This version can't handle '-R {:>4.2}'.", r);
+        }
         match s.solve() {
             Ok(Certificate::SAT(v)) => {
                 if let Ok(out) = File::create(&result) {
@@ -59,5 +65,7 @@ fn main() {
             }
             Err(_) => println!("Failed"),
         }
+    } else {
+        println!("{} does not exist.", args.cnf.file_name().unwrap().to_str().unwrap());
     }
 }
