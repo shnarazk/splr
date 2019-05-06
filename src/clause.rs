d0cc6e531c6a27e30d4798d1f451b45748bc420c
shujinarazaki <shujinarazaki@protonmail.com>
2019-05-06 23:23:24 +0900

    reduce(clause.rs): keep JUST_USED

--- src/clause.rs
+++ src/clause.rs
@@ -433,8 +433,7 @@ impl ClauseDBIF for ClauseDB {
             let c = &mut clause[*i];
             if c.is(Flag::JUST_USED) {
                 c.turn_off(Flag::JUST_USED)
-            }
-            if 2 < c.rank {
+            } else if 2 < c.rank {
                 c.kill(touched);
             }
         }
