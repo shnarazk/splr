d295085a07d432fb0b220d8f2da0c1197871b184
shujinarazaki <shujinarazaki@protonmail.com>
2019-05-06 23:24:20 +0900

    State(state.rs): add force_restart_by_stagnation and check in force_restart(resart.rs)

--- src/state.rs
+++ src/state.rs
@@ -176,6 +176,7 @@ pub struct State {
     pub luby_restart_factor: f64,
     pub use_deep_search_mode: bool,
     pub stagnated: bool,
+    pub force_restart_by_stagnation: bool,
     /// Eliminator
     pub use_elim: bool,
     /// 0 for no limit
@@ -331,6 +332,7 @@ impl Default for State {
             luby_restart_factor: 100.0,
             use_deep_search_mode: true,
             stagnated: false,
+            force_restart_by_stagnation: false,
             ema_coeffs: (2 ^ 5, 2 ^ 15),
             use_elim: true,
             elim_eliminate_combination_limit: 80,
