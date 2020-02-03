b0f03b265e21c86ddd6b7fddf9f7375be93d3fc2
Narazaki, Shuji <shujinarazaki@protonmail.com>
2020-02-04 00:14:07 +0900

    lib.rs: update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,11 @@
 Splr is a pure Rustic SAT solver, based on [Glucose 4.1](https://www.labri.fr/perso/lsimon/glucose/).
 It adopts various research results on SAT solvers:
 
-- CDCL, watch literals, VSIDS and so on from [Minisat](http://minisat.se) and the ancestors
+- CDCL, watch literals, and so on from [Minisat](http://minisat.se) and the ancestors
 - Glucose-like dynamic blocking/forcing restarts based on [EMAs](https://arxiv.org/abs/1506.08905)
 - heuristics adaptation
 - pre/in-process simplification based on clause subsumption and variable elimination
+- Learning Rate Based Branching and Reason Side Rewarding
 
 *Many thanks to SAT researchers.*
 
