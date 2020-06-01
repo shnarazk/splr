4b39412834d0db8ac9afa87c814710b92dbbb613
Narazaki, Shuji <shujinarazaki@protonmail.com>
2020-06-02 01:12:44 +0900

    fix typos
    	modified:   src/solver/search.rs

--- src/solver/search.rs
+++ src/solver/search.rs
@@ -138,7 +138,7 @@ impl SolveIF for Solver {
         match answer {
             Ok(true) => {
                 // As a preparation for incremental solving, we need to backtrack to the
-                // root level. So all assgingments, including assignments to eliminated vars,
+                // root level. So all assigngments, including assignments to eliminated vars,
                 // are stored in an extra storage. It has the same type of `AssignStack::assign`.
                 let model = asg.extend_model(cdb, elim.eliminated_lits());
                 #[cfg(debug)]
@@ -154,7 +154,7 @@ impl SolveIF for Solver {
                     }
                 }
 
-                // Run valitator on the extended model.
+                // Run validator on the extended model.
                 if cdb.validate(&model, false).is_some() {
                     return Err(SolverError::SolverBug);
                 }
