7c5be89ce2ecea1e687107f3bcea01035edde983
Narazaki, Shuji <shujinarazaki@protonmail.com>
2020-06-22 08:08:20 +0900

    (propagate) change the condition of 'best' assignments
    	modified:   src/assign/propagate.rs

--- src/assign/propagate.rs
+++ src/assign/propagate.rs
@@ -291,7 +291,7 @@ impl PropagateIF for AssignStack {
             }
         }
         let na = self.q_head + self.num_eliminated_vars;
-        if self.num_best_assign < na && 0 < self.decision_level() {
+        if self.num_best_assign <= na && 0 < self.decision_level() {
             self.best_assign = true;
             self.num_best_assign = na;
         }
