f0f231dfd62162490e5357215e0439a5e5f7e0c9
Narazaki, Shuji <shujinarazaki@protonmail.com>
2020-07-06 01:53:37 +0900

    implement another vivification algorithm
    	modified:   src/solver/vivify.rs

--- src/solver/vivify.rs
+++ src/solver/vivify.rs
@@ -8,9 +8,10 @@ use {
         state::StateIF,
         types::*,
     },
+    std::collections::HashSet,
 };
 
-pub fn vivify(asg: &mut AssignStack, cdb: &mut ClauseDB, state: &mut State) {
+pub fn vivify_old(asg: &mut AssignStack, cdb: &mut ClauseDB, state: &mut State) {
     asg.handle(SolverEvent::Vivify(true));
     state[Stat::Vivification] += 1;
     let display_step: usize = 1_000;
@@ -147,4 +148,211 @@ pub fn vivify(asg: &mut AssignStack, cdb: &mut ClauseDB, state: &mut State) {
         nassign, nshrink, nclause,
     ));
     asg.handle(SolverEvent::Vivify(false));
+}
+
+/// vivify clauses in `cdb` under `asg`
+pub fn vivify(asg: &mut AssignStack, cdb: &mut ClauseDB, state: &mut State) {
+    asg.handle(SolverEvent::Vivify(true));
+    state[Stat::Vivification] += 1;
+    let display_step: usize = 1_000;
+    let check_thr = state.vivify_thr as usize;
+    let mut ncheck = 0;
+    let mut nclause = 0;
+    let mut nshrink = 0;
+    let mut nassign = 0;
+    let mut to_display = display_step;
+    debug_assert_eq!(asg.decision_level(), asg.root_level);
+    if asg.propagate(cdb) != ClauseId::default() {
+        panic!("eabarmbrr");
+    }
+    let mut clauses: Vec<ClauseId> = Vec::new();
+    for (i, c) in cdb.iter_mut().enumerate().skip(1) {
+        if !c.is(Flag::DEAD) && 2 < c.len() {
+            clauses.push(ClauseId::from(i));
+        }
+    }
+    clauses.sort_by_cached_key(|c| {
+        cdb[c]
+            .iter()
+            .map(|l| (asg.activity(l.vi()) * 100_000.0) as usize)
+            .max()
+            .unwrap()
+    });
+    'next_clause: for ci in clauses.iter().rev() {
+        let reward = cdb.activity(*ci);
+        let c: &Clause = &cdb[ci];
+        // Since GC can make `clauses` out of date, we need to check its aliveness here.
+        if c.is(Flag::DEAD) {
+            continue;
+        }
+        nclause += 1;
+        let is_learnt = c.is(Flag::LEARNT);
+        let clits = c.lits.clone();
+        let mut v: Vec<Lit> = Vec::new();
+        let mut vivified: Vec<Lit> = Vec::new();
+        for (i, l) in clits.iter().enumerate() {
+            ncheck += 1;
+            if to_display <= ncheck {
+                state.flush("");
+                state.flush(format!(
+                    "vivifying(fix:{}, strengthen:{}/{}, check:{}/{})...",
+                    nassign, nshrink, nclause, ncheck, check_thr,
+                ));
+                to_display = ncheck + display_step;
+            }
+            match asg.assigned(*l) {
+                Some(true) => {
+                    let mut r: Vec<Lit> = match asg.reason(l.vi()) {
+                        AssignReason::None => continue 'next_clause,
+                        AssignReason::Implication(cid, _) => cdb[cid].lits.to_vec(),
+                    };
+                    for lit in &mut r {
+                        if *lit == *l {
+                            *lit = !*l;
+                            break;
+                        }
+                    }
+                    v.push(!*l);
+                    vivified = asg.remove_redundant(cdb, &v, &r); // Rule 2
+                    assert!(0 < vivified.len());
+                    continue 'next_clause;
+                    break;
+                }
+                Some(false) => {
+                    v.push(!*l); // Rule 4, // continue
+                }
+                None => {
+                    let cid: Option<ClauseId> = match v.len() {
+                        0 => None,
+                        1 => {
+                            asg.assign_by_decision(v[0]);
+                            None
+                        }
+                        _ => {
+                            let mut vv = v.clone();
+                            Some(cdb.new_clause(asg, &mut vv, false, false)) // L.12
+                        }
+                    };
+                    asg.assign_by_decision(!*l);
+                    let confl = asg.propagate(cdb);
+                    v.push(!*l);
+                    if confl == ClauseId::default() {
+                        if let Some(cj) = cid {
+                            cdb.detach(cj);
+                            cdb.garbage_collect();
+                        }
+                        asg.cancel_until_sandbox(asg.root_level);
+                    } else {
+                        let avivified = asg.remove_redundant(cdb, &v, &cdb[confl].lits); // Rule 3
+                        assert!(
+                            0 < avivified.len(),
+                            format!(
+                                "empty vivified, v.len: {}, nassign: {}",
+                                v.len(),
+                                asg.stack_len(),
+                            ),
+                        );
+                        if let Some(cj) = cid {
+                            cdb.detach(cj);
+                            cdb.garbage_collect();
+                        }
+                        asg.cancel_until_sandbox(asg.root_level);
+                        // continue 'next_clause;
+                        break;
+                    }
+                }
+            }
+        }
+        if vivified.is_empty() {
+            for l in &mut v {
+                vivified.push(!*l);
+            }
+        }
+        match vivified.len() {
+            0 => {
+                // Is this possible? The only posibility is the clause is falsified.
+                // This mean we find the CNF is unsatisfiable.
+                // So terminate vivification immediately and put the responsibility on solver!
+                break 'next_clause;
+            }
+            1 => {
+                nassign += 1;
+                asg.assign_at_rootlevel(vivified[0]).expect("impossible");
+                asg.handle(SolverEvent::Fixed);
+                let confl = asg.propagate(cdb);
+                if confl != ClauseId::default() {
+                    panic!("eabarmbrr");
+                }
+            }
+            _ => {
+                nshrink += 1;
+                let cj = cdb.new_clause(asg, &mut vivified, is_learnt, false);
+                cdb.set_activity(cj, reward);
+            }
+        }
+        cdb.detach(*ci);
+        cdb.garbage_collect();
+        if check_thr < ncheck {
+            break;
+        }
+    }
+    if state.config.vivify_end <= state.vivify_thr {
+        state.vivify_thr = state.config.vivify_beg;
+    } else {
+        state.vivify_thr *= 2.0;
+    }
+    state.flush("");
+    state.flush(format!(
+        "vivified(fix:{} & strengthen:{} of clauses:{})...",
+        nassign, nshrink, nclause,
+    ));
+    asg.handle(SolverEvent::Vivify(false));
+}
+
+impl AssignStack {
+    fn reason_literals<'a>(&self, cdb: &'a ClauseDB, l: Lit) -> &'a [Lit] {
+        match self.reason(l.vi()) {
+            AssignReason::Implication(cid, _) => &cdb[cid].lits,
+            AssignReason::None => &cdb[ClauseId::default()].lits,
+        }
+    }
+
+    /// inspect the complete implication graph to collect a disjunction of a subset of
+    /// negated literals of `clause`
+    fn remove_redundant(&mut self, cdb: &ClauseDB, clause: &[Lit], reason: &[Lit]) -> Vec<Lit> {
+        let mut seen: HashSet<Lit> = HashSet::new();
+        let mut res: Vec<Lit> = Vec::new();
+        for l in reason {
+            seen.insert(*l);
+        }
+        for l in self.stack_iter().rev() {
+            // l is a negated literal
+            if !seen.contains(&!*l) && !seen.contains(l) {
+                continue;
+            }
+            if clause.contains(l) || clause.contains(&!*l) {
+                res.push(*l);
+                continue;
+            }
+            for r in self.reason_literals(cdb, *l).iter() {
+                seen.insert(*r);
+            }
+        }
+        /*
+        if reason.len() == 2 {
+            println!(
+                "{:?} /\\ {} {:?} => {:?}",
+                clause,
+                if let Some(_) = self.assigned(clause[0]) {
+                    "assigned"
+                } else {
+                    "unassigned"
+                },
+                reason,
+                res
+            );
+        }
+        */
+        res
+    }
 }
