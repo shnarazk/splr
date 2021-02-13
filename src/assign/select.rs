74fd117b0b3cb5c20ed7683e1858667f1f9fde0f
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-02-13 09:43:07 +0900

    So var boosting based on best phase is used as the default scheme

--- src/assign/select.rs
+++ src/assign/select.rs
@@ -90,7 +90,7 @@ impl VarSelectIF for AssignStack {
             self.stage_mode_select += 1;
             let n = self.num_unreachables().next_power_of_two().trailing_zeros() as usize;
             match self.stage_mode_select % n {
-                2 => target = StagingTarget::Best(0),
+                0 => target = StagingTarget::Best(0),
                 8 => target = StagingTarget::Random,
                 // 3 => target = StagingTarget::Core,
                 // 4 => target = StagingTarget::LastAssigned,
