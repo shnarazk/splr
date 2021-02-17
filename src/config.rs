29103625d7e43296bdb8b187324935b6cfbad7e0
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-02-18 08:09:57 +0900

    change the default value of `vrw_dcy_rat`

--- src/config.rs
+++ src/config.rs
@@ -231,7 +231,7 @@ impl Default for Config {
             rst_stb_exp: 1.0,
             rst_stb_scl: 2.0,
 
-            stg_rwd_dcy: 0.998,
+            stg_rwd_dcy: 0.95,
             stg_rwd_val: 1.0,
 
             viv_thr: 1,
