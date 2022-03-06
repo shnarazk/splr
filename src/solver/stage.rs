70425a8263500253f7629dbe7140fcc51da05177
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-03-07 01:33:49 +0900

    quick fix

--- src/solver/stage.rs
+++ src/solver/stage.rs
@@ -84,7 +84,9 @@ impl StageManager {
         self.cycle * self.unit_size
     }
     pub fn num_reducible(&self) -> usize {
-        self.current_span()
+        let span = self.current_span();
+        let keep = 2 * (span as f64).sqrt() as usize;
+        span - keep
     }
     pub fn current_stage(&self) -> usize {
         self.stage
