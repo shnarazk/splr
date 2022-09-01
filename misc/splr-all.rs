468de91d0940104158be94fe5a33b8e44a656b44
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-09-01 23:58:09 +0900

    modified:   misc/splr-all.rs

--- misc/splr-all.rs
+++ misc/splr-all.rs
@@ -1,4 +1,9 @@
 #![cfg(feature = "incremental_solver")]
+/// All solutions solver implementation based on feature 'incremental solver'
+/// But the main purpose is to check the correctness of the implementaion of
+/// feature 'incremental solver'.
+///
+/// To run me:
 ///```ignore
 /// cargo run --example splr-incremental -- a.cnf
 ///```
@@ -6,17 +11,25 @@ use splr::*;
 use std::env::args;
 
 fn main() {
-    let cnf = args().nth(1).expect("takes an arg");
-    let ns = run(&cnf);
+    let cnf_file = args().nth(1).expect("takes an arg");
+    let ns = run(&cnf_file);
     println!("#solution: {}", ns);
 }
 
-fn run(cnf: &str) -> usize {
-    let mut solver = Solver::try_from(cnf).expect("panic");
+fn run(cnf_file: &str) -> usize {
+    let mut solver = Solver::try_from(cnf_file).expect("panic");
+    // let cnf = solver.state.config.cnf();
     let mut count = 0;
-    for _res in solver.iter() {
+    let mut solutions = Vec::new();
+    for res in solver.iter() {
+        solutions.push(res);
         count += 1;
         println!("s SATISFIABLE({})", count);
+        // dump_refuting_cnf(cnf, res);
     }
     count
 }
+
+// fn dump_refuting_cnf(cnf: CNF, clause: Vec<i32>) {
+//     todo!();
+// }
