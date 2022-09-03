1024bdca15001f61868ec338e71bab54850e096f
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-09-03 12:43:29 +0900

    modified:   misc/splr-all.rs

--- misc/splr-all.rs
+++ misc/splr-all.rs
@@ -1,5 +1,7 @@
 #![cfg(feature = "incremental_solver")]
 use ::cnf::*;
+use splr::*;
+use std::env::args;
 /// All solutions solver implementation based on feature 'incremental solver'
 /// But the main purpose is to check the correctness of the implementaion of
 /// feature 'incremental solver'.
@@ -8,8 +10,6 @@ use ::cnf::*;
 ///```ignore
 /// cargo run --example all-solutions -- a.cnf
 ///```
-use splr::*;
-use std::env::args;
 
 fn main() {
     let cnf_file = args().nth(1).expect("takes an arg");
@@ -18,21 +18,18 @@ fn main() {
 }
 
 fn run(cnf_file: &str) -> usize {
-    let cnf = CNF::load(&std::path::PathBuf::from(cnf_file)).expect("fail to load");
+    let mut cnf = CNF::load(&std::path::PathBuf::from(cnf_file)).expect("fail to load");
     println!("{cnf}");
     let mut solver = Solver::try_from(cnf_file).expect("panic");
-    // let cnf = solver.state.config.cnf();
     let mut count = 0;
     let mut solutions = Vec::new();
     for res in solver.iter() {
-        solutions.push(res);
         count += 1;
-        println!("s SATISFIABLE({})", count);
-        // dump_refuting_cnf(cnf, res);
+        let refuter: Vec<i32> = res.iter().map(|l| -l).collect::<Vec<_>>();
+        solutions.push(res);
+        cnf.add_clause(refuter).expect("An internal error");
+        println!("s SATISFIABLE({count}) => {cnf}");
     }
+    // cnf.save(???);???;
     count
 }
-
-// fn dump_refuting_cnf(cnf: CNF, clause: Vec<i32>) {
-//     todo!();
-// }
