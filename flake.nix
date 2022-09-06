fbaf16a6e73844362a3dec9c0490907721ac2900
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-09-06 19:36:30 +0900

    update Nix flake files

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
                 stdenv.mkDerivation rec {
                   name = "splr-${version}";
                   pname = "splr";
-                  version = "0.15.0-20220515";
+                  version = "0.16.0-20220906";
                   src = self;
                   buildInputs = [ cargo libiconv rustc binutils ];
                   buildPhase = "cargo build --release";
