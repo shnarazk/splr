3740d034248a991107222923b4166d0defaf1d40
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-09-07 00:48:17 +0900

    cargo test

--- src/cnf/mod.rs
+++ src/cnf/mod.rs
@@ -17,15 +17,15 @@ mod tests {
         assert!(cnf.add_clause(vec![-1, -4, 3]).is_ok());
         assert_eq!(cnf.num_clauses(), 2);
         assert_eq!(cnf.num_vars(), 4);
-        let output = cnf.to_string();
+        let output = cnf.dump_to_string();
         let mut line = output.lines();
         assert_eq!(line.next(), Some("p cnf 4 2"));
-        assert_eq!(line.next(), Some("1 3 2"));
-        assert_eq!(line.next(), Some("-1 -4 3"));
+        assert_eq!(line.next(), Some("1 3 2 0"));
+        assert_eq!(line.next(), Some("-1 -4 3 0"));
     }
     #[test]
     fn test_load_uf8() {
-        let build = CNF::load(Path::new("../cnfs/uf8.cnf"));
+        let build = CNF::load(Path::new("cnfs/uf8.cnf"));
         dbg!(build.is_ok());
         let cnf = build.unwrap();
         assert_eq!(cnf.num_clauses(), 13);
@@ -33,7 +33,7 @@ mod tests {
     }
     #[test]
     fn test_load_sample() {
-        let build = CNF::load(Path::new("../cnfs/sample.cnf"));
+        let build = CNF::load(Path::new("cnfs/sample.cnf"));
         dbg!(build.is_ok());
         let cnf = build.unwrap();
         assert_eq!(cnf.num_clauses(), 1065);
