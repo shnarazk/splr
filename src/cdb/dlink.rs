2b3e6eacf4ef5c09a089ac3fdc08ca127d61f40f
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-04-14 02:07:40 +0900

    modified:   src/cdb/dlink.rs

--- src/cdb/dlink.rs
+++ src/cdb/dlink.rs
@@ -19,6 +19,7 @@ pub struct LinkHead {
 
 pub struct DancingLinks {
     pub watch: Vec<LinkHead>,
+    pub element: Vec<DoubleLink>,
 }
 
 impl Index<Lit> for DancingLinks {
@@ -40,21 +41,39 @@ pub trait DancingLinkIF {
     type Element;
     fn next_watcher(&mut self, lit: Lit) -> usize;
     fn prev_watcher(&mut self, lit: Lit) -> usize;
-    fn insert(&mut self, index: usize);
-    fn free_element(&mut self) -> Option<usize>;
+    fn insert_watcher(&mut self, lit: Lit, index: usize);
+    fn remove_watcher(&mut self, lit: Lit, index: usize);
+    fn get_free_watcher(&mut self) -> Option<usize>;
 }
 
 impl DancingLinkIF for DancingLinks {
     type Element = Clause;
     fn next_watcher(&mut self, lit: Lit) -> usize {
-        self[lit].next
+        self.watch[usize::from(lit)].next
     }
     fn prev_watcher(&mut self, lit: Lit) -> usize {
-        return 0;
+        self.watch[usize::from(lit)].prev
     }
-    fn insert(&mut self, index: usize) {}
-    fn free_element(&mut self) -> Option<usize> {
-        return None;
+    fn insert_watcher(&mut self, lit: Lit, index: usize) {
+        let prev = self.watch[usize::from(lit)].prev;
+        self.element[prev].next = index;
+        self.element[index].prev = prev;
+        self.element[index].next = 0;
+        self.watch[usize::from(lit)].next = index;
+    }
+    fn remove_watcher(&mut self, lit: Lit, index: usize) {
+        let next = self.watch[usize::from(lit)].next;
+        let prev = self.watch[usize::from(lit)].prev;
+        self.element[prev].next = next;
+        self.element[next].prev = prev;
+        let p = self.element[1].prev;
+        self.element[p].next = index;
+        self.element[index].prev = p;
+        self.element[index].next = 0;
+    }
+    fn get_free_watcher(&mut self) -> Option<usize> {
+        let index = self.watch[1].next;
+        (index != 0).then_some(index)
     }
 }
 
