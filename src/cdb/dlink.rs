63108ad0f3256d08d1080f4d6646bf3ada736e6b
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-04-14 10:49:38 +0900

    modified:   src/cdb/dlink.rs

--- src/cdb/dlink.rs
+++ src/cdb/dlink.rs
@@ -3,81 +3,139 @@ use {
     std::ops::{Index, IndexMut},
 };
 
+type ClauseIndex = usize;
+
 #[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct DoubleLink {
-    pub prev: usize,
-    pub next: usize,
+    pub prev: ClauseIndex,
+    pub next: ClauseIndex,
+}
+
+pub struct Clause {
+    pub link0: DoubleLink,
+    pub link1: DoubleLink,
+    pub lits: Vec<Lit>,
+}
+
+impl Clause {
+    fn next_for_lit(&self, lit: Lit) -> ClauseIndex {
+        if self.lits[0] == lit {
+            self.link0.next;
+        }
+        if self.lits[1] == lit {
+            self.link1.next;
+        }
+        panic!("ilegal chain")
+    }
+    fn next_for_lit_mut(&mut self, lit: Lit) -> &mut ClauseIndex {
+        if self.lits[0] == lit {
+            &mut self.link0.next;
+        }
+        if self.lits[1] == lit {
+            &mut self.link1.next;
+        }
+        panic!("ilegal chain")
+    }
+    fn prev_for_lit(&self, lit: Lit) -> ClauseIndex {
+        if self.lits[0] == lit {
+            self.link0.prev;
+        }
+        if self.lits[1] == lit {
+            self.link1.prev;
+        }
+        panic!("ilegal chain")
+    }
+    fn prev_for_lit_mut(&self, lit: Lit) -> &mut ClauseIndex {
+        if self.lits[0] == lit {
+            &mut self.link0.prev;
+        }
+        if self.lits[1] == lit {
+            &mut self.link1.prev;
+        }
+        panic!("ilegal chain")
+    }
+    fn erase_links(&mut self) {
+        self.link0.prev = 0;
+        self.link0.next = 0;
+        self.link1.prev = 0;
+        self.link1.next = 0;
+    }
 }
 
 #[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct LinkHead {
-    pub prev: usize,
-    pub next: usize,
-    pub count: usize,
-    pub timestamp: usize,
+    pub prev: ClauseIndex,
+    pub next: ClauseIndex,
+    pub count: ClauseIndex,
+    pub timestamp: ClauseIndex,
 }
 
-pub struct DancingLinks {
-    pub watch: Vec<LinkHead>,
-    pub element: Vec<DoubleLink>,
+pub struct ClauseDB {
+    pub header: Vec<LinkHead>,
+    pub clause: Vec<Clause>,
 }
 
-impl Index<Lit> for DancingLinks {
-    type Output = LinkHead;
+impl Index<Lit> for ClauseDB {
+    type Output = Clause;
     #[inline]
     fn index(&self, lit: Lit) -> &Self::Output {
-        &self.watch[usize::from(lit)]
+        &self.clause[ClauseIndex::from(lit)]
     }
 }
 
-impl IndexMut<Lit> for DancingLinks {
+impl IndexMut<Lit> for ClauseDB {
     #[inline]
     fn index_mut(&mut self, lit: Lit) -> &mut Self::Output {
-        &mut self.watch[usize::from(lit)]
+        &mut self.clause[ClauseIndex::from(lit)]
     }
 }
 
-pub trait DancingLinkIF {
-    type Element;
-    fn next_watcher(&mut self, lit: Lit) -> usize;
-    fn prev_watcher(&mut self, lit: Lit) -> usize;
-    fn insert_watcher(&mut self, lit: Lit, index: usize);
-    fn remove_watcher(&mut self, lit: Lit, index: usize);
-    fn get_free_watcher(&mut self) -> Option<usize>;
+pub trait DancingIndexIF {
+    fn get_watcher_link(&mut self, lit: Lit) -> ClauseIndex;
+    fn get_free_watcher(&mut self) -> ClauseIndex;
+    fn insert_watcher(&mut self, lit: Lit, index: ClauseIndex);
+    fn remove_watcher(&mut self, lit: Lit, index: ClauseIndex);
 }
 
-impl DancingLinkIF for DancingLinks {
-    type Element = Clause;
-    fn next_watcher(&mut self, lit: Lit) -> usize {
-        self.watch[usize::from(lit)].next
+const HEAD_INDEX: ClauseIndex = 0;
+const FREE_INDEX: ClauseIndex = 1;
+
+impl DancingIndexIF for ClauseDB {
+    fn get_watcher_link(&mut self, lit: Lit) -> ClauseIndex {
+        self.clause[ClauseIndex::from(lit)].next_for_lit(lit)
     }
-    fn prev_watcher(&mut self, lit: Lit) -> usize {
-        self.watch[usize::from(lit)].prev
+    fn get_free_watcher(&mut self) -> ClauseIndex {
+        let index = self.header[FREE_INDEX].next;
+        if index != 0 {
+            let next = self.clause[FREE_INDEX].link0.next;
+            self.header[FREE_INDEX].next = next;
+            self.clause[index].erase_links();
+            index
+        } else {
+            self.clause.push(Clause::default());
+            1 + self.clause.len()
+        }
     }
-    fn insert_watcher(&mut self, lit: Lit, index: usize) {
-        let prev = self.watch[usize::from(lit)].prev;
-        self.element[prev].next = index;
-        self.element[index].prev = prev;
-        self.element[index].next = 0;
-        self.watch[usize::from(lit)].next = index;
+    fn insert_watcher(&mut self, lit: Lit, index: ClauseIndex) {
+        let last = self.header[ClauseIndex::from(lit)].prev;
+        *self.clause[last].next_for_lit_mut(lit) = index;
+        *self.clause[index].prev_for_lit_mut(lit) = last;
+        *self.clause[index].next_for_lit_mut(lit) = HEAD_INDEX;
+        self.header[ClauseIndex::from(lit)].prev = index;
     }
-    fn remove_watcher(&mut self, lit: Lit, index: usize) {
-        let next = self.watch[usize::from(lit)].next;
-        let prev = self.watch[usize::from(lit)].prev;
-        self.element[prev].next = next;
-        self.element[next].prev = prev;
-        let p = self.element[1].prev;
-        self.element[p].next = index;
-        self.element[index].prev = p;
-        self.element[index].next = 0;
-    }
-    fn get_free_watcher(&mut self) -> Option<usize> {
-        let index = self.watch[1].next;
-        (index != 0).then_some(index)
+    fn remove_watcher(&mut self, lit: Lit, index: ClauseIndex) {
+        let next = self.clause[ClauseIndex::from(lit)].next_for_lit(lit);
+        let prev = self.clause[ClauseIndex::from(lit)].prev_for_lit(lit);
+        *self.clause[prev].next_for_lit_mut(lit) = next;
+        *self.clause[next].prev_for_lit_mut(lit) = prev;
+        let p = self.header[FREE_INDEX].prev;
+        self.clause[p].link0.next = index;
+        self.clause[index].link0.prev = p;
+        self.clause[index].link0.next = HEAD_INDEX;
     }
 }
 
-impl Instantiate for DancingLinks {
+impl Instantiate for ClauseDB {
     fn instantiate(conf: &Config, cnf: &CNFDescription) -> Self {
         unimplemented!()
     }
