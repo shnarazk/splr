add5db8dba173102089db7ecbb8f5f98ee1f0d4f
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-07-06 20:11:40 +0900

    design dancing weaver interface: src/cdb/weaver.rs

--- src/cdb/weaver.rs
+++ src/cdb/weaver.rs
@@ -1,9 +1,14 @@
-use {crate::types::*, std::collections::HashSet};
+use crate::types::*;
+
+#[derive(Debug, Default, Eq, Ord, PartialEq, PartialOrd)]
+pub struct WatchLiteralIndexRef {
+    prev: WatchLiteralIndex,
+    next: WatchLiteralIndex,
+}
 
 pub trait WatcherLinkIF {
     fn next_watch(&self, wi: usize) -> WatchLiteralIndex;
     fn next_watch_mut(&mut self, wi: usize) -> &mut WatchLiteralIndex;
-    // fn swap_watch_orders(&mut self);
 }
 
 /// Note: this interface is based on ClauseIndex and Watch literal's position
@@ -15,30 +20,14 @@ pub trait ClauseWeaverIF {
     fn get_free_index(&mut self) -> ClauseIndex;
     /// insert a watch to a watch list`
     fn insert_watch(&mut self, ci: ClauseIndex, wi: usize);
-    /// O(1) remove the *next* watch literal index of the watch literal index `wli`
-    fn remove_next_watch(
-        &mut self,
-        wli: WatchLiteralIndex,
-        next: WatchLiteralIndex,
-        lit: Lit,
-    ) -> WatchLiteralIndex;
-    /// O(n) remove function which remove the clause refered from `wli`
+    /// O(1) remove the watch literal index of the watch literal index `wli`
+    fn remove_watch(&mut self, wli: WatchLiteralIndex, lit: Lit) -> WatchLiteralIndex;
+    /// O(1) remove function which remove the clause from two watcher lists
     fn remove_watches(&mut self, ci: ClauseIndex);
     /// instantiate a list of watch lists
     fn make_watches(num_vars: usize, clauses: &mut [Clause]) -> Vec<WatchLiteralIndex>;
-    /// un-register a clause `cid` from alive clause database and make the clause dead.
-    fn nullify_clause(&mut self, ci: ClauseIndex, deads: &mut HashSet<Lit>);
-    /// un-register a clause `cid` from clause database and make the clause dead.
-    fn nullify_clause_sandbox(&mut self, ci: ClauseIndex, deads: &mut HashSet<Lit>);
-    /// update watches of the clause
-    fn reweave(&mut self, targets: &mut HashSet<Lit>);
-    /// link a clause `ci` to free list
-    fn free_clause(&mut self, ci: ClauseIndex);
+    /// unlink and link to freelist
+    fn unweave(&mut self, ci: ClauseIndex);
+    /// unlink and link to freelist
+    fn unweave_sandbox(&mut self, ci: ClauseIndex);
 }
-
-// #[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
-// pub struct LinkHead {
-//     pub next: ClauseIndex,
-//     pub count: ClauseIndex,
-//     pub timestamp: ClauseIndex,
-// }
