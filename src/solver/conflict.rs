1b12907ee865e166286c1ca01d572abc8c6f73b0
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-07-15 15:29:53 +0900

    call `minimize_with_bi_clauses` without checking clause size

--- src/solver/conflict.rs
+++ src/solver/conflict.rs
@@ -499,9 +499,7 @@ fn minimize_learnt(
     }
     let l0 = new_learnt[0];
     new_learnt.retain(|l| *l == l0 || !l.is_redundant(asg, cdb, &mut to_clear, &levels));
-    if (2..=32).contains(&new_learnt.len()) {
-        cdb.minimize_with_bi_clauses(asg, new_learnt);
-    }
+    cdb.minimize_with_bi_clauses(asg, new_learnt);
     // find correct backtrack level from remaining literals
     let mut level_to_return = 0;
     if 1 < new_learnt.len() {
