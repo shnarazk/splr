162ffeac5c38d909b9a8988c446ddbaa64b14b41
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-03-01 21:26:40 +0900

    fix: reimplement OrderedProxy::cmp noy to panic by sort

--- src/types.rs
+++ src/types.rs
@@ -660,7 +660,8 @@ impl<T: Clone + Default + PartialEq + Ord> PartialOrd for OrderedProxy<T> {
 
 impl<T: Clone + Default + PartialEq + Ord> Ord for OrderedProxy<T> {
     fn cmp(&self, other: &OrderedProxy<T>) -> Ordering {
-        if (self.index - other.index).abs() < f64::EPSILON {
+        if self.index == other.index {
+            // if (self.index - other.index).abs() < f64::EPSILON {
             self.body.cmp(&other.body)
         } else if self.index < other.index {
             Ordering::Less
