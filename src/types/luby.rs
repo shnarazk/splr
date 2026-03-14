266408bae9e39d1a03e55d788fb99b85604cad36
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-03-14 21:55:17 +0900

    feat(Luby): implement LubySequence

--- src/types/luby.rs
+++ src/types/luby.rs
@@ -1,4 +1,66 @@
+//! O(1) Luby sequence implementation based on https://github.com/shnarazk/LubySequence
 use std::{fmt, num::NonZeroU32};
+
+#[derive(Clone, Debug)]
+pub struct LubySegment {
+    pub seg_index: u32,
+    pub ix_in_seg: u32,
+}
+
+impl Default for LubySegment {
+    fn default() -> Self {
+        LubySegment {
+            seg_index: 1,
+            ix_in_seg: 0,
+        }
+    }
+}
+
+impl fmt::Display for LubySegment {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Luby[seg:{}, ix:{}]", self.seg_index, self.ix_in_seg)
+    }
+}
+
+impl LubySegment {
+    pub fn segment_len(&self) -> u32 {
+        self.seg_index.trailing_zeros() + 1
+    }
+    pub fn luby(&self) -> u32 {
+        2_u32.pow(self.ix_in_seg)
+    }
+    pub fn to_next(&self) -> Self {
+        if self.ix_in_seg + 1 == self.segment_len() {
+            Self {
+                seg_index: self.seg_index + 1,
+                ix_in_seg: 0,
+            }
+        } else {
+            Self {
+                seg_index: self.seg_index,
+                ix_in_seg: self.ix_in_seg + 1,
+            }
+        }
+    }
+    pub fn shift(&mut self) -> &mut Self {
+        if self.ix_in_seg + 1 == self.segment_len() {
+            self.seg_index += 1;
+            self.ix_in_seg = 0;
+        } else {
+            self.ix_in_seg += 1;
+        }
+        self
+    }
+}
+
+impl Iterator for LubySegment {
+    type Item = Self;
+    /// Find the finite subsequence that contains index 'x', and the
+    /// size of that subsequence as: 1, 1, 2, 1, 1, 2, 4, 1, 1, 2, 1, 1, 2, 4, 8
+    fn next(&mut self) -> Option<Self> {
+        Some(self.to_next())
+    }
+}
 
 #[derive(Clone, Debug)]
 pub struct LubySeries {
@@ -93,10 +155,33 @@ mod tests {
     #[test]
     fn test_luby_series() {
         let mut luby = LubySeries::default();
+        // Note: Luby sequence starts with 1, 1, 2, ...
+        // We can't take the first value in the following iteration.
         let v = vec![1, 2, 1, 1, 2, 4, 1, 1, 2, 1, 1, 2, 4, 8];
         let l = v
             .iter()
             .map(|_| luby.next_unchecked())
+            .collect::<Vec<usize>>();
+        assert_eq!(l, v);
+    }
+    #[test]
+    fn test_luby_segment1() {
+        let mut luby = LubySegment::default();
+        assert_eq!(luby.seg_index, 1);
+        assert_eq!(luby.ix_in_seg, 0);
+        assert_eq!(luby.luby(), 1);
+        luby.shift();
+        assert_eq!(luby.seg_index, 2);
+        assert_eq!(luby.ix_in_seg, 0);
+        assert_eq!(luby.luby(), 1);
+    }
+    #[test]
+    fn test_luby_segment2() {
+        let mut luby = LubySegment::default();
+        let v = vec![1, 2, 1, 1, 2, 4, 1, 1, 2, 1, 1, 2, 4, 8];
+        let l = v
+            .iter()
+            .map(|_| luby.shift().luby() as usize)
             .collect::<Vec<usize>>();
         assert_eq!(l, v);
     }
