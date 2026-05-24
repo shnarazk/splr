a8c3314057e839be809e50d084ae637efa65521e
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-05-24 16:56:23 +0900

    feat: remove FlagClause::YOUNG

--- src/types/flags.rs
+++ src/types/flags.rs
@@ -25,8 +25,6 @@ bitflags! {
         const OCCUR_LINKED  = 0b0000_0100;
         /// a clause is registered in vars' assing list.
         const ASSIGN_REASON = 0b0000_1000;
-        /// a clause born after last reduction.
-        const YOUNG         = 0b0001_0000;
     }
 }
 
